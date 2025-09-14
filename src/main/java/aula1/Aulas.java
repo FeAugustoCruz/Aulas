adb0cc73a2e39562bc965c7454e0066797920bd9
Fernando <81120858+FeAugustoCruz@users.noreply.github.com>
2025-09-13 21:58:00 -0300

    inital commit

---
+++ src/main/java/aula1/Aulas.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package aula1;
+
+/**
+ *
+ * @author Fernando
+ */
+public class Aulas {
+
+    public static void main(String[] args) {
+        /*
+        Declaração normal:
+        */
+        int idade = 3;
+        float salario = 1835.43f;
+        char letra = 'A';
+        boolean casada = false;
+        
+        /*
+        Declaração (TypeCast):
+        */
+        int idade2 = (int) 3;
+        float salario2 = (float) 1825.22;
+        char letra2 = (char) 'A';
+        boolean casada2 = (boolean) false;
+        
+        /*
+        Declaração (Wrapper class):
+        */
+        Integer idade3 = 3;
+        Float salario3 = 1242.12f;
+        Character letra3 = 'A';
+        Boolean casada3 = false;
+        
+    }
+}
