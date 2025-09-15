6a04ef089055500c70285eaa7220c8e2a4dac100
Fernando <81120858+FeAugustoCruz@users.noreply.github.com>
2025-09-15 18:02:04 -0300

    commit 15/09/2025

--- src/main/java/aula2/Aulas2.java
+++ src/main/java/aula2/Aulas2.java
@@ -3,6 +3,14 @@ package aula2;
 
 public class Aulas2 {
     public static void main(String[] args) {
-        System.out.println("Hoa0");
+        int n1 = 10;
+        int n2 = 30;
+        /*
+        Lógica de execução dos calculos aritméticos
+        */
+        float media = (n1 + n2)/2;
+        System.out.println("A media e: " + media);
+        
+        
     }
 }
