afaf8cc213712550598a2193e8e20e0f5f6df6a5
Fernando <81120858+FeAugustoCruz@users.noreply.github.com>
2025-09-17 00:36:29 -0300

    commit 17/09/2025

---
+++ src/main/java/aula3/Aula3.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula3;
+
+/**
+ *
+ * @author Fernando
+ */
+public class Aula3 {
+    public static void main(String[] args) {
+        System.out.println("Ola mundo!");
+    }
+}
