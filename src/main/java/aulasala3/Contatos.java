0ef03c1900f1b2956b4fe2441de8887d0c030e91
Fernando <81120858+FeAugustoCruz@users.noreply.github.com>
2025-10-09 17:27:59 -0300

    commit

---
+++ src/main/java/aulasala3/Contatos.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aulasala3;
+
+/**
+ *
+ * @author Fernando
+ */
+public class Contatos {
+    private String nome;
+    private int telefone;
+}
---
+++ src/main/java/aulasala3/Sala.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aulasala3;
+
+/**
+ *
+ * @author Fernando
+ */
+public class Sala {
+   aulasala3.Quadro quadro = new aulasala3.Quadro(); 
+   //ligação entre classes!
+}
