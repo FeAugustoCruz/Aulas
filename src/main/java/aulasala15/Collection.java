9bd271b0ad8179116d12cb9dd32bff0770ad224e
Fernando <81120858+FeAugustoCruz@users.noreply.github.com>
2025-11-26 16:30:05 -0300

    new commit

---
+++ src/main/java/aulasala15/Collection.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aulasala15;
+//é um tamplate que define metodos comuns para qualuqer collection
+/**
+ *
+ * @author Fernando
+ */
+public class Collection {
+    
+}
