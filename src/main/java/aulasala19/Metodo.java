8705fa186771fa387583ed9774201bdaae0ce2cb
Fernando <81120858+FeAugustoCruz@users.noreply.github.com>
2026-01-21 18:34:56 -0300

    aula 19

---
+++ src/main/java/aulasala19/Metodo.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aulasala19;
+
+/**
+ *
+ * @author Fernando
+ */
+//Forma de um metodo de lançar uma exeção para outro método dar try catch
+//RunTimeException -> erros que não necessitam de um try catch
+//Exception -> necessita de um tyr catch
+public class Metodo {
+    
+    public static void metodo() throws{
+        new java.io.FileNotFoundException("arquivo.txt");
+    }
+    
+    public void a() throws FileNotFoundException{
+        try{
+            metodo();
+        } catch (java.io.FileNotFoundException e){
+            System.out.println("Arquivo nçao encontrado");
+        }
+    }
+}
